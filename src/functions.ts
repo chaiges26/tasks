6843921dc60897ef424cf1b3e20d8c55220c911a
chaiges26 <chaiges@udel.edu>
2022-02-16 17:10:31 -0500

    last 3 functions done

--- src/functions.ts
+++ src/functions.ts
@@ -33,7 +33,8 @@ export function add3(first: number, second: number, third: number): number {
  * mark added to the end.
  */
 export function shout(message: string): string {
-    return "";
+    const caps = message.toUpperCase() + "!";
+    return caps;
 }
 
 /**
@@ -41,7 +42,7 @@ export function shout(message: string): string {
  * mark. Do not use an `if` statement in solving this question.
  */
 export function isQuestion(message: string): boolean {
-    return true;
+    return message.endsWith("?");
 }
 
 /**
@@ -50,5 +51,13 @@ export function isQuestion(message: string): boolean {
  * upper or lower case), then return `false`. Otherwise, return `null`.
  */
 export function convertYesNo(word: string): boolean | null {
-    return true;
+    let bool = true;
+    if (word.toUpperCase() === "YES") {
+        bool = true;
+    } else if (word.toUpperCase() === "NO") {
+        bool = false;
+    } else {
+        bool = null;
+    }
+    return bool;
 }
